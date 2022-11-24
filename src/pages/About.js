8d5b2870ca34bccf1f6f175316d75d81b8fe8730
PravinST23 <pravin4893@gmail.com>
2022-11-24 11:16:53 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+import MultiplePizzas from "../assets/multiplePizzas.jpeg";
+import "../styles/About.css";
+function About() {
+  return (
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${MultiplePizzas})` }}
+      ></div>
+      <div className="aboutBottom">
+        <h1> ABOUT US</h1>
+        <p>
+        The company itself is a very successful company. Most of all, the inflexibility, the inconveniences that even the benefits of rejecting result from the pleasures of the labors, never let any one of his flattery but hate to flee by the mere flight of the present option, and those things! They provide for such accusers, but no one. Blinded by the truth, he is bound by the right of them to trouble the architect with some pleasure, something will happen or the fault of the services! He is hindered from seeking, hates, is held in error, these do not know the very debts which something. He will criticize, because Where is the error of rejecting the flight? But are they expedient to receive the troubles of those who praise them in a wise manner? But some people refuse to get the most out of these other mistakes. You see, the little ones don't know pain! The pleasures of the law of things from whomever he wills
+
+
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Contact.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PizzaLeft from "../assets/pizzaLeft.jpg";
+import "../styles/Contact.css";
+
+function Contact() {
+  return (
+    <div className="contact">
+      <div
+        className="leftSide"
+        style={{ backgroundImage: `url(${PizzaLeft})` }}
+      ></div>
+      <div className="rightSide">
+        <h1> Contact Us</h1>
+
+        <form id="contact-form" method="POST">
+          <input name="name" placeholder="Enter full name..." type="text" />
+          <input name="email" placeholder="Enter email..." type="email" />
+          <textarea
+            rows="6"
+            placeholder="Enter message..."
+            name="message"
+            required
+          ></textarea>
+          <button type="submit"> Send Message</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
